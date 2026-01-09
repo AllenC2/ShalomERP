ad9518244cd37f05aa67f2084eef9a0979d249b1
Allen Contreras <allencontreras@Aliens-MacBook.local>
2026-01-09 13:51:59 -0600

    Correccion para acceder a log-viewer

---
+++ app/Http/Middleware/LogViewerBasicAuth.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class LogViewerBasicAuth
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        $username = env('LOG_VIEWER_USERNAME');
+        $password = env('LOG_VIEWER_PASSWORD');
+
+        if (!$username || !$password) {
+            return $next($request);
+        }
+
+        if ($request->getUser() !== $username || $request->getPassword() !== $password) {
+            return response('Unauthorized', 401, ['WWW-Authenticate' => 'Basic']);
+        }
+
+        return $next($request);
+    }
+}
